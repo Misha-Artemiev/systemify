49fea3769561f7de0df0a52a50afad1873f3b3b8
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-03 16:23:25 +0000

    simple cli input with optional output

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,2 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct CLIInput {
+    private var input: String? {readLine()}
+    public func string() -> String? {
+        return input
+    }
+    public func int() -> Int? {
+        guard (input != nil) else {return nil}
+        return Int(input!)
+    }
+    public func double() -> Double? {
+        guard (input != nil) else {return nil}
+        return Double(input!)
+    }
+    public func float() -> Float? {
+        guard (input != nil) else {return nil}
+        return Float(input!)
+    }
+    public func bool() -> Bool? {
+        guard (input != nil) else {return nil}
+        return Bool(input!)
+    }
+}
