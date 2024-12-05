24d676b910376967351a79eeb2be34cc1d81ed33
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-05 11:22:57 +0000

    added bool input

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,33 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct CLIInput_old {
-    private var input: String? {readLine()}
-    public func string() -> String? {
-        return input
-    }
-    public func toInt() -> Int? {
-        guard (input != nil) else {return nil}
-        return Int(input!)
-    }
-    
-    public func toDouble() -> Double? {
-        guard (input != nil) else {return nil}
-        return Double(input!)
-    }
-    public func toFloat() -> Float? {
-        guard (input != nil) else {return nil}
-        return Float(input!)
-    }
-    public func toBool() -> Bool? {
-        guard (input != nil) else {return nil}
-        return Bool(input!)
-    }
-    public func toEnum<T: CaseIterable & Hashable>(_ enumType: T.Type) -> T? {
-        return enumType.allCases.first { "\(String(describing: $0))" == input }
-    }
-}
-
 public func CLIInput<T>(_ type: T.Type) -> T {
     let input = readLine()
     switch type.self {
@@ -42,7 +15,8 @@ public func CLIInput<T>(_ type: T.Type) -> T {
     case is Float.Type:
         return "as" as! T
     case is Bool.Type:
-        return "as" as! T
+        guard let toReturn: T = Bool(input!) as? T else { return false as! T}
+        return toReturn
     default:
         return "as" as! T
     }
