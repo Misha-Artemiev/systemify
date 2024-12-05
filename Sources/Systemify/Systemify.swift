9576212baf5117010ef5d11cc284e800a6ca3eb6
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-05 12:27:11 +0000

    fixed float and double just in case

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -11,10 +11,10 @@ public func CLIInput<T>(_ type: T.Type) -> T {
         guard let toReturn: T = input as? T else { return "" as! T}
         return toReturn
     case is Double.Type, is Double?.Type:
-        guard let toReturn: T = Double(input!) as? T else { return 0 as! T}
+        guard let toReturn: T = Double(input!) as? T else { return Double(0) as! T}
         return toReturn
     case is Float.Type, is Float?.Type:
-        guard let toReturn: T = Float(input!) as? T else { return 0 as! T}
+        guard let toReturn: T = Float(input!) as? T else { return Float(0) as! T}
         return toReturn
     case is Bool.Type, is Bool?.Type:
         guard let toReturn: T = Bool(input!.lowercased()) as? T else { return false as! T}
