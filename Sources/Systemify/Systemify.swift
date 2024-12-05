c1470d3845f4da87bb8cbe189e53f42c7eb6bd17
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-05 09:54:25 +0000

    test cliinput lib

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,25 +1,45 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct CLIInput {
+public struct CLIInput_old {
     private var input: String? {readLine()}
     public func string() -> String? {
         return input
     }
-    public func int() -> Int? {
+    public func toInt() -> Int? {
         guard (input != nil) else {return nil}
         return Int(input!)
     }
-    public func double() -> Double? {
+    
+    public func toDouble() -> Double? {
         guard (input != nil) else {return nil}
         return Double(input!)
     }
-    public func float() -> Float? {
+    public func toFloat() -> Float? {
         guard (input != nil) else {return nil}
         return Float(input!)
     }
-    public func bool() -> Bool? {
+    public func toBool() -> Bool? {
         guard (input != nil) else {return nil}
         return Bool(input!)
+    }
+    public func toEnum<T: CaseIterable & Hashable>(_ enumType: T.Type) -> T? {
+        return enumType.allCases.first { "\(String(describing: $0))" == input }
+    }
+}
+
+public func CLIInput<T>(_ type: T) -> T {
+    var input = readLine() ?? ""
+    switch type {
+    case is Int:
+        return Int(input) as! T
+    case is Double:
+        return "as" as! T
+    case is Float:
+        return "as" as! T
+    case is Bool:
+        return "as" as! T
+    default:
+        return "as" as! T
     }
 }
