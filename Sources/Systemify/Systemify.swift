371c29d82303dabe8554b1000cec71561e17e956
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-09 22:05:45 +0000

    another magic solution

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,80 +1,41 @@
 import Foundation
 
 public enum CLIInputKind<T> {
-    case type
+    case int
+    case optionalInt
+    case string
+    case optionalString
+    case character
+    case optionalCharacter
+    case double
+    case optionalDouble
+    case float
+    case optionalFloat
+    case bool
+    case optionalBool
 }
 
-// Provide discoverable typed options:
-extension CLIInputKind where T == Int {
-    static var int: CLIInputKind<Int> { .type }
-}
-
-extension CLIInputKind where T == Int? {
-    static var optionalInt: CLIInputKind<Int?> { .type }
-}
-
-extension CLIInputKind where T == String {
-    static var string: CLIInputKind<String> { .type }
-}
-
-extension CLIInputKind where T == String? {
-    static var optionalString: CLIInputKind<String?> { .type }
-}
-
-extension CLIInputKind where T == Character {
-    static var character: CLIInputKind<Character> { .type }
-}
-
-extension CLIInputKind where T == Character? {
-    static var optionalCharacter: CLIInputKind<Character?> { .type }
-}
-
-extension CLIInputKind where T == Double {
-    static var double: CLIInputKind<Double> { .type }
-}
-
-extension CLIInputKind where T == Double? {
-    static var optionalDouble: CLIInputKind<Double?> { .type }
-}
-
-extension CLIInputKind where T == Float {
-    static var float: CLIInputKind<Float> { .type }
-}
-
-extension CLIInputKind where T == Float? {
-    static var optionalFloat: CLIInputKind<Float?> { .type }
-}
-
-extension CLIInputKind where T == Bool {
-    static var bool: CLIInputKind<Bool> { .type }
-}
-
-extension CLIInputKind where T == Bool? {
-    static var optionalBool: CLIInputKind<Bool?> { .type }
-}
 
 public func CLIInput<T>(_ type: CLIInputKind<T>) -> T {
     let input = readLine()
     switch type {
-    case .type where T.self == Int.self, .type where T.self == Optional<Int>.self:
+    case .int, .optionalInt:
         guard let toReturn: T = Int(input!) as? T else { return 0 as! T}
         return toReturn
-    case .type where T.self == String.self, .type where T.self == String?.self:
+    case .string, .optionalString:
         guard let toReturn: T = input as? T else { return "" as! T}
         return toReturn
-    case .type where T.self == Character.self, .type where T.self == Character?.self:
+    case .character, .optionalCharacter:
         guard let toReturn: T = Character(input!) as? T else { return "" as! T}
         return toReturn
-    case .type where T.self == Double.self, .type where T.self == Double?.self:
+    case .double, .optionalDouble:
         guard let toReturn: T = Double(input!) as? T else { return Double(0) as! T}
         return toReturn
-    case .type where T.self == Float.self, .type where T.self == Float?.self:
+    case .float, .optionalFloat:
         guard let toReturn: T = Float(input!) as? T else { return Float(0) as! T}
         return toReturn
-    case .type where T.self == Bool.self, .type where T.self == Bool?.self:
+    case .bool, .optionalBool:
         guard let toReturn: T = Bool(input!.lowercased()) as? T else { return false as! T}
         return toReturn
-    default:
-        return "" as! T
     }
 }
