3078cd7927fbab3c925f00735b4b78cafc10b4f3
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-09 21:18:53 +0000

    test with enum insted of general generic

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,49 +1,35 @@
 import Foundation
 
-// int/?,string/?,double/?,float/?,bool/?
-public func CLIInput<T>(_ type: T.Type) -> T {
-    let input = readLine()
-    switch type.self {
-    case is Int.Type, is Int?.Type:
-        guard let toReturn: T = Int(input!) as? T else { return 0 as! T}
-        return toReturn
-    case is String.Type, is String?.Type:
-        guard let toReturn: T = input as? T else { return "" as! T}
-        return toReturn
-    case is Double.Type, is Double?.Type:
-        guard let toReturn: T = Double(input!) as? T else { return Double(0) as! T}
-        return toReturn
-    case is Float.Type, is Float?.Type:
-        guard let toReturn: T = Float(input!) as? T else { return Float(0) as! T}
-        return toReturn
-    case is Bool.Type, is Bool?.Type:
-        guard let toReturn: T = Bool(input!.lowercased()) as? T else { return false as! T}
-        return toReturn
-    default:
-        return "" as! T
-    }
+public enum CLIInputTypes<T> {
+    case int(Int)
+    case intOptional(Int?)
+    case string(String)
+    case stringOptional(String?)
+    case double(Double)
+    case doubleOptional(Double?)
+    case float(Float)
+    case floatOptional(Float?)
+    case bool(Bool)
+    case boolOptional(Bool?)
 }
 
-public struct FileSystem {
-    var currentPath: URL = URL("")!
-    
-    func cd(path: String) {
-        
-    }
-    
-    func ls() {
-        
-    }
-    
-    func mkdir(name: String) {
-        
-    }
-    
-    func rm(name: String, isFolder: Bool = false) {
-        
-    }
-    
-    func touch(name: String) {
-        
+public func CLIInput<T>(_ type: CLIInputTypes<T>) -> T {
+    let input = readLine()
+    switch type {
+    case .int, .intOptional:
+        guard let toReturn: T = Int(input!) as? T else { return 0 as! T}
+        return toReturn
+    case .string, .stringOptional:
+        guard let toReturn: T = input as? T else { return "" as! T}
+        return toReturn
+    case .double, .doubleOptional:
+        guard let toReturn: T = Double(input!) as? T else { return Double(0) as! T}
+        return toReturn
+    case .float, .floatOptional:
+        guard let toReturn: T = Float(input!) as? T else { return Float(0) as! T}
+        return toReturn
+    case .bool, .boolOptional:
+        guard let toReturn: T = Bool(input!.lowercased()) as? T else { return false as! T}
+        return toReturn
     }
 }
