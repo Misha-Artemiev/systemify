4a0c6f178b64a92715342b62efa43956de974750
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2024-12-09 22:39:03 +0000

    got back and added character

--- Sources/Systemify/Systemify.swift
+++ Sources/Systemify/Systemify.swift
@@ -1,25 +1,31 @@
 import Foundation
 
 public func CLIInput<T>(_ type: T.Type) -> T {
-    let input = readLine()
+    let input = readLine() ?? ""
     switch type.self {
     case is Int.Type, is Int?.Type:
-        guard let toReturn: T = Int(input!) as? T else { return 0 as! T}
+        guard let toReturn: T = Int(input) as? T else { return 0 as! T}
         return toReturn
     case is String.Type, is String?.Type:
-        guard let toReturn: T = input as? T else { return "" as! T}
+        if input.isEmpty {
+            guard let toReturn: T = nil else { return "" as! T}
+        }
+        guard let toReturn: T = Character(input) as? T else { return "" as! T}
         return toReturn
     case is Character.Type, is Character?.Type:
-        guard let toReturn: T = Character(input!) as? T else { return "" as! T}
+        if input.isEmpty {
+            guard let toReturn: T = nil else { return " " as! T}
+        }
+        guard let toReturn: T = Character(input) as? T else { return " " as! T}
         return toReturn
     case is Double.Type, is Double?.Type:
-        guard let toReturn: T = Double(input!) as? T else { return Double(0) as! T}
+        guard let toReturn: T = Double(input) as? T else { return Double(0) as! T}
         return toReturn
     case is Float.Type, is Float?.Type:
-        guard let toReturn: T = Float(input!) as? T else { return Float(0) as! T}
+        guard let toReturn: T = Float(input) as? T else { return Float(0) as! T}
         return toReturn
     case is Bool.Type, is Bool?.Type:
-        guard let toReturn: T = Bool(input!.lowercased()) as? T else { return false as! T}
+        guard let toReturn: T = Bool(input.lowercased()) as? T else { return false as! T}
         return toReturn
     default:
         return "" as! T
